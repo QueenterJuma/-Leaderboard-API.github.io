8a47b52b9eb5b9d4828b053472c7bd69fdf3c782
QueenterJuma <jumahqueenter@gmail.com>
2023-03-09 13:10:39 +0300

    Add update in index.js file and fixed linter errors

--- src/modules/btn_refresh.js
+++
@@ -1,8 +0,0 @@
-const btnReset = () => {
-  const restore = document.querySelector(".refresh-btn");
-  restore.addEventListener('click', () => {
-    location.reload();
-  });
-};
-
-export default btnReset;
